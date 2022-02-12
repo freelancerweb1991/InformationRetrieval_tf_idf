18a54a52ded3d82a2ae705ba074bf3ff7bf76b53
Farhad S <freelancerweb1991@gmail.com>
2022-02-12 13:28:54 +0330

    Initial

---
+++ PY/dataProvider.py
@@ -0,0 +1,90 @@
+import os
+import sqlite3  
+ 
+dataPath = 'data\hotels\data'
+dbPath = 'data\hotels\dataBase.db' 
+
+# this function initialize database for store stracture data.
+def initializeDataBase():
+    conn = sqlite3.connect(dbPath)
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS hotels (id INTEGER PRIMARY KEY AUTOINCREMENT, city TEXT, name TEXT)')
+    cur.execute('CREATE TABLE IF NOT EXISTS comments (id INTEGER PRIMARY KEY AUTOINCREMENT, hotelId INTEGER, date DATE, comment TEXT)')
+    conn.commit()
+    cur.close()
+
+def storeFilesOnDatabase():
+    totalHotelsCount = 0
+    totalCommentsCount = 0
+    conn = sqlite3.connect(dbPath)
+    cur = conn.cursor()
+    for fileName in os.walk(dataPath): 
+            splitLength = fileName[0].split('\\').__len__()
+            city = fileName[0].split('\\')[splitLength-1]   
+            for file in fileName[2] :
+                comments = []
+                totalHotelsCount += 1
+                hotelName = file.split(city,1)[1].replace('_',' ').strip().capitalize()
+                cur.execute('INSERT INTO hotels (city, name) VALUES (?,?)', (city, hotelName))
+                hotelId = cur.lastrowid 
+                #print('**',city,'**','**', hotelName,'***************************** ',fileName[0],'/',file) 
+                with open(os.path.join(f'{fileName[0]}/{file}')) as f:
+                    cn = f.readlines()
+                for line in cn: 
+                    totalCommentsCount += 1
+                    date = line.split('\t')[0]
+                    comment = line.replace(date,'')
+                    comments.append(( hotelId, date.strip(), comment.strip()))
+
+                if(comments.__len__() > 0):
+                    cur.executemany('INSERT INTO comments (hotelId, date, comment) VALUES (?,?,?)', comments)
+                    
+            
+    print('# hotelsCount:', totalHotelsCount,' CommentsCount:', totalCommentsCount,'#########################################################################')
+    conn.commit()
+    cur.close()
+
+def getHotels():
+    conn = sqlite3.connect(dbPath)
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM hotels') 
+    rows = cur.fetchall()
+    print('*** HOTELS ***********************************************************')
+    for row in rows:
+        print(row)
+    conn.commit()
+    cur.close()
+
+def getHotelsCount():
+    conn = sqlite3.connect(dbPath)
+    cur = conn.cursor()
+    cur.execute('SELECT count(1) FROM hotels')
+    hotelsCount = cur.fetchone()[0]
+    conn.commit()
+    cur.close()
+    return hotelsCount
+
+def getComments():
+    conn = sqlite3.connect(dbPath)
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM comments LIMIT 2') 
+    rows = cur.fetchall()
+
+    """  
+    print('*** COMMENTS ***********************************************************') 
+    for row in rows:
+        print(row[0],'  ',row[1],'  ', row[2],'  ', row[3][:75])  
+    """
+    
+    conn.commit()
+    cur.close()
+    return rows
+ 
+def getCommentsCount():
+    conn = sqlite3.connect(dbPath)
+    cur = conn.cursor()
+    cur.execute('SELECT count(1) FROM comments LIMIT 20')
+    commentsCount = cur.fetchone()[0]
+    conn.commit()
+    cur.close()
+    return commentsCount
---
+++ PY/tokenizer.py
@@ -0,0 +1,91 @@
+import datetime
+from fileinput import filename
+import sqlite3   
+import dataProvider as dataProvider
+import nltk
+import re
+
+
+
+corpus = {}
+dbPathRI = 'data\hotels\dataBaseRI.db'
+def initializeDataBaseRI():
+    conn = sqlite3.connect(dbPathRI)
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS terms (id INTEGER PRIMARY KEY AUTOINCREMENT, token TEXT, frequeny INTEGER, info TEXT)') 
+    conn.commit()
+    cur.close()
+
+def tokenizeComents():
+    startTime = datetime.datetime.now()
+    print ('Creatin RI database start time:', startTime)
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+    lemmatizer = nltk.stem.WordNetLemmatizer()
+    ps = nltk.stem.PorterStemmer()
+
+    for comment in dataProvider.getComments():
+        cmnt = re.sub('[^a-zA-Z]',' ',comment[3]) 
+        allTokens = [word.lower() for word in nltk.word_tokenize(cmnt)]
+        filteredTokens = [word for word in allTokens if not word in stop_words]
+        lemmatizedTokens = [lemmatizer.lemmatize(word) for word in filteredTokens]
+        stemmedTokens = [ps.stem(word) for word in lemmatizedTokens]
+        
+        """ for i in filteredTokens:
+            print(i) """
+        freq = {word: stemmedTokens.count(word) for word in set(stemmedTokens)}
+
+        updateTokens(comment[0], freq)  
+
+        print ('** allTokens:',allTokens.__len__(),' ** after deleting stop words filteredTokens:', filteredTokens.__len__(), ' --', lemmatizedTokens.__len__(), ' --', stemmedTokens.__len__())
+
+    
+    endTime = datetime.datetime.now()
+    print('complete creating RI database:', endTime)
+    print('Total duration:', endTime - startTime)    
+
+def updateTokens(docId, terms):
+    conn = sqlite3.connect(dbPathRI)
+    cur = conn.cursor()
+    frmtupdt= ',{}:{}'
+    for term in terms:
+        cur.execute('SELECT count(1) FROM terms WHERE token = ?',(term,))
+        isExisted = cur.fetchone()[0]
+        if(isExisted > 0):
+            cur.execute('UPDATE terms SET info = info + ? where token= ?  ',(frmtupdt.format(docId,terms[term]), term)) 
+        else:
+            frmtupdt= '{}:{}'
+            cur.execute('INSERT INTO terms (token, info) VALUES (?, ?)',(frmtupdt.format(docId,terms[term]), term))
+    conn.commit()
+    cur.close()
+
+def getTerms(isPrintResult):
+    conn = sqlite3.connect(dbPathRI)
+    cur = conn.cursor()
+    cur.execute('SELECT id, token, info FROM terms order by token')
+    rows = cur.fetchall()
+
+    if(isPrintResult):
+        for row in rows:
+            print(row)
+        if(rows.__len__() == 0):
+            print ('terms is empty table.')
+
+    conn.commit()
+    cur.close()
+    return rows
+
+def updateTokens1(term):
+    conn = sqlite3.connect(dbPathRI)
+    cur = conn.cursor()
+    frmtupdt= ',{}:{}'
+    cur.execute('SELECT count(1) FROM terms WHERE token == ?',(term,))
+    isExisted = cur.fetchone()[0]
+    if(isExisted > 0):
+        print('update .......') 
+    else:
+        print('insert .......') 
+    conn.commit()
+    cur.close()
+
+
+
